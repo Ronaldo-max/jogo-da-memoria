f7118d9c114520d91b1866f1a2fa3e935d5a4479
Ronaldo-max <ronaldodcs21@gmail.com>
2022-03-03 22:57:26 -0300

    correção de bugs

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
+# JOGO DA MEMÓRIA
+
+<img src="" alt="jogo da memória" />
+
+## Tecnologias
+
+- HTML
+- CSS
+- JAVASCRIPT
+
+## Imagens e audio
+
+### Icons
+
 <div>Ícones feitos por <a href="https://www.flaticon.com/br/autores/flat-icons" title="Flat Icons">Flat Icons</a> from <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com</a></div>
 
+### Audio
+
 <div>
-<span>Mariachi Snooze de Kevin MacLeod é licenciada de acordo com a licença Atribuição 4.0 da Creative Commons. <a href="https://creativecommons.org/licenses/by/4.0/">https://creativecommons.org/licenses/by/4.0/</a></span>
-
-<span>Fonte: <a href="http://incompetech.com/music/royalty-free/index.html?isrc=USUAN1100166">http://incompetech.com/music/royalty-free/index.html?isrc=USUAN1100166</a></span>
-
-<span>Artista: <a href="http://incompetech.com/">http://incompetech.com/</a></span>
-
+<p>Mariachi Snooze de Kevin MacLeod é licenciada de acordo com a licença Atribuição 4.0 da Creative Commons. <a href="https://creativecommons.org/licenses/by/4.0/">https://creativecommons.org/licenses/by/4.0/</a></p>
+<p>Fonte: <a href="http://incompetech.com/music/royalty-free/index.html?isrc=USUAN1100166">http://incompetech.com/music/royalty-free/index.html?isrc=USUAN1100166</a></p>
+<p>Artista: <a href="http://incompetech.com/">http://incompetech.com/</a></p>
 </div>
+
+## Visualização
+
+Jogo da memória: <a href="https://ronaldo-max.github.io/jogo-da-memoria/">https://ronaldo-max.github.io/jogo-da-memoria/</a>
+
+## Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const cards = document.querySelectorAll(".card");
 const time = document.querySelector(".timer");
 const completeCardTag = document.querySelector(".cardsNumber");
-const buttonRefresh = document.querySelector("#refresh");
-const buttonAudio = document.querySelector("#audio");
+const buttonRefresh = document.getElementById("refresh");
+const buttonAudio = document.querySelector("#buttonAudio");
 const audio = document.querySelector("audio");
 const imagePlay = document.getElementById("play");
 const imagePause = document.getElementById("pause");
 
 let timer = 30;
-let timerFunc = timer;
+let timerAll = timer;
 let firstCard;
 let secondCard;
 let disableCards = false;
@@ -17,125 +17,121 @@ let playGame = false;
 let timerInterval;
 let completeCard = 0;
 let countAudio = 0;
-let autoPlayAudio;
 
-function playAudio() {
-    audio.play();
-    countAudio = 1;
-    audio.loop = true;
-    imagePause.style.display = "none";
-    imagePlay.style.display = "flex";
-}
-
-function playAndPause() {
-    if (countAudio == 1) {
-        audio.pause();
-        audio.loop = false;
-        audio.currentTime = 0;
-        clearInterval(autoPlayAudio);
-        imagePause.style.display = "flex";
-        imagePlay.style.display = "none";
-        countAudio = 0;
-    } else {
-        playAudio();
-    }
-}
-
-function moveCardTimer() {
-    if (timerFunc <= 0) {
-        return clearInterval(timerInterval);
-    }
-    timerFunc--;
-    time.innerHTML = "Timer: " + timerFunc;
-}
-
-function moveCard(e) {
-    if (!playGame) {
-        playGame = true;
-        timerInterval = setInterval(moveCardTimer, 1000);
-    }
-    let moveClick = e.target;
-
-    if (moveClick !== firstCard && !disableCards && timerFunc > 0) {
-        completeCard++;
-        completeCardTag.innerHTML = "Cards: " + completeCard;
-        moveClick.classList.add("move");
-        if (!firstCard) {
-            return (firstCard = moveClick);
-        }
-
-        secondCard = moveClick;
-        disableCards = true;
-        let firstCardImg = firstCard.querySelector("img#back").src;
-        let secondCardImg = secondCard.querySelector("img#back").src;
-
-        setCards(firstCardImg, secondCardImg);
-    }
-}
-
-function setCards(img1, img2) {
-    if (img1 === img2) {
-        count++;
-
-        if (count == 8 && timerFunc > 0) {
+const cardFunction = {
+    moveCardTimer() {
+        if (timerAll <= 0) {
             return clearInterval(timerInterval);
         }
+        timerAll--;
+        time.innerHTML = "Timer: " + timerAll;
+    },
 
-        firstCard.removeEventListener("click", moveCard);
-        secondCard.removeEventListener("click", moveCard);
-        firstCard = secondCard = "";
-        return (disableCards = false);
-    }
+    moveCard(e) {
+        if (!playGame) {
+            playGame = true;
+            timerInterval = setInterval(cardFunction.moveCardTimer, 1000);
+        }
+        let moveClick = e.target;
 
-    setTimeout(() => {
-        firstCard.classList.add("shake");
-        secondCard.classList.add("shake");
-    }, 400);
+        if (moveClick !== firstCard && !disableCards && timerAll > 0) {
+            completeCard++;
+            completeCardTag.innerHTML = "Cards: " + completeCard;
+            moveClick.classList.add("move");
+            if (!firstCard) {
+                return (firstCard = moveClick);
+            }
 
-    setTimeout(() => {
-        firstCard.classList.remove("shake", "move");
-        secondCard.classList.remove("shake", "move");
+            secondCard = moveClick;
+            disableCards = true;
+            let firstCardImg = firstCard.querySelector("img#back").src;
+            let secondCardImg = secondCard.querySelector("img#back").src;
+
+            cardValueImage.setCards(firstCardImg, secondCardImg);
+        }
+    },
+};
+
+const cardValueImage = {
+    setCards(img1, img2) {
+        if (img1 === img2) {
+            count++;
+
+            if (count == 8 && timerAll > 0) {
+                return clearInterval(timerInterval);
+            }
+
+            firstCard.removeEventListener("click", cardFunction.moveCard);
+            secondCard.removeEventListener("click", cardFunction.moveCard);
+            firstCard = secondCard = "";
+            return (disableCards = false);
+        }
+
+        setTimeout(() => {
+            firstCard.classList.add("shake");
+            secondCard.classList.add("shake");
+        }, 100);
+
+        setTimeout(() => {
+            firstCard.classList.remove("shake", "move");
+            secondCard.classList.remove("shake", "move");
+            firstCard = secondCard = "";
+            disableCards = false;
+        }, 500);
+    },
+};
+
+const actionsButtons = {
+    playAndPause() {
+        if (countAudio == 1) {
+            audio.pause();
+            countAudio = 0;
+            audio.loop = false;
+            audio.currentTime = 0;
+            imagePause.style.display = "flex";
+            imagePlay.style.display = "none";
+        } else {
+            audio.play();
+            countAudio = 1;
+            audio.loop = true;
+            imagePause.style.display = "none";
+            imagePlay.style.display = "flex";
+        }
+    },
+
+    upgradeDeck() {
+        timerAll = timer;
+        time.innerHTML = "Tempo: " + timerAll;
+        clearInterval(timerInterval);
+        completeCard = 0;
+        completeCardTag.innerHTML = "Cartas: " + completeCard;
+        count = 0;
         firstCard = secondCard = "";
         disableCards = false;
-    }, 1200);
-}
+        playGame = false;
 
-function upgradeDeck() {
-    timerFunc = timer;
-    time.innerHTML = "Tempo: " + timerFunc;
-    clearInterval(timerInterval);
-    completeCard = 0;
-    completeCardTag.innerHTML = "Cartas: " + completeCard;
-    count = 0;
-    firstCard = secondCard = "";
-    disableCards = false;
-    playGame = false;
+        let images = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
+        images.sort(() => (Math.random() > 0.5 ? 1 : -1));
 
-    let images = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-    images.sort(() => (Math.random() > 0.5 ? 1 : -1));
+        let past = [1, 2, 3, 4, 5, 6, 7, 8];
+        past = Math.floor(Math.random() * 9);
 
-    let past = [1, 2, 3, 4, 5, 6, 7, 8];
-    past = Math.floor(Math.random() * past.length);
+        let pastNumber = past === 0 ? 1 : past;
 
-    let pastNumber = past === 0 ? 1 : past;
+        cards.forEach((card, index) => {
+            card.classList.remove("move");
+            let img = card.querySelector("#back");
+            img.src = `./assets/images_${pastNumber}/image_${images[index]}.png`;
+            card.addEventListener("click", cardFunction.moveCard);
+        });
+    },
+};
 
-    cards.forEach((card, index) => {
-        card.classList.remove("move");
-        let img = card.querySelector("#back");
-        img.src = `./assets/images_${pastNumber}/image_${images[index]}.png`;
-        card.addEventListener("click", moveCard);
-    });
-}
+actionsButtons.upgradeDeck();
 
-autoPlayAudio = setInterval(() => {
-    playAudio();
-}, 100);
-
-upgradeDeck();
-
-buttonRefresh.addEventListener("click", upgradeDeck);
-buttonAudio.addEventListener("click", playAndPause);
+buttonRefresh.addEventListener("click", actionsButtons.upgradeDeck);
+buttonAudio.addEventListener("click", actionsButtons.playAndPause);
 
 cards.forEach((card) => {
-    card.addEventListener("click", moveCard);
+    card.addEventListener("click", cardFunction.moveCard);
 });
